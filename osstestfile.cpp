b4df21c8628ef841fd513cd9022dd2b92130091f
piyushsir <91408589+piyushsir@users.noreply.github.com>
2023-07-31 15:37:20 +0530

    Add files via upload

---
+++ osstestfile.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=10;
+    if(n&1)
+    {
+        cout<<"odd"<<endl;
+    }
+    else
+    {
+
+        cout<<"even"<<endl;
+    }
+}
